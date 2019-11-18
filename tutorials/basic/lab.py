49394d0c9f26a007f660754b82659568dcf0eaee
Dmitry Selivanov <st022657@student.spbu.ru>
2019-11-18 19:11:18 +0300

    minor

--- tutorials/basic/lab.py
+++ tutorials/basic/lab.py
@@ -2,19 +2,19 @@ from madt_lib.network import Network
 
 
 def main():
-    madt = Network('15.0.0.0/8')
+    net = Network('15.0.0.0/8')
     # create network nodes that will represent client and server
-    server = madt.create_node('server', image='madt/nginx')
-    client = madt.create_node('client', image='inutano/wget',
+    server = net.create_node('server', image='madt/nginx')
+    client = net.create_node('client', image='inutano/wget',
                             entrypoint='sh -c "while true; do wget -O - -T 3 $SERVER; sleep 1; done"')
     # create a local network that will connect all those nodes
-    madt.create_subnet('net', (server, client))
+    net.create_subnet('net', (server, client))
     # distribute IP addresses
-    madt.configure(verbose=True)
+    net.configure(verbose=True)
     # pass server IP to the client
     client.add_options(environment={'SERVER': server.get_ip()})
     # save lab
-    madt.render('../../labs/basic_tutorial', verbose=True)
+    net.render('../../labs/basic_tutorial', verbose=True)
 
 
 if __name__ == "__main__":
--- tutorials/docker_compose/lab.py
+++ tutorials/docker_compose/lab.py
@@ -1,26 +1,33 @@
 from madt_lib.network import Network, Overlay
-kt = Network('15.0.0.0/8')
 
-# create network nodes that will represent client and server
-server = kt.create_node('server', image='madt/docker_compose', enable_internet=True, privileged=True)
-clients = kt.generate_nodes('client', 6, image='madt/pyget', environment={"SERVER": 'whoami.local'})
 
-with open('docker-compose.yml') as docker_compose_yml_file:
-    server.add_dir('.', '/app')
+def main():
+    net = Network('15.0.0.0/8')
 
-# create a local network that will connect all those nodes
-routers = kt.generate_nodes('router', 3)
-kt.create_subnet('subnet1', (routers[0], *clients[:3]))
-kt.create_subnet('subnet2', (routers[1], *clients[3:]))
-kt.create_subnet('subnet3', routers)
-kt.create_subnet('subnet4', (routers[2], server))
+    # create network nodes that will represent client and server
+    server = net.create_node('server', image='madt/docker_compose', enable_internet=True, privileged=True)
+    clients = net.generate_nodes('client', 6, image='madt/pyget', environment={"SERVER": 'whoami.local'})
 
-kt.create_overlay(Overlay.RIP, 'RIP', routers)
+    with open('docker-compose.yml') as docker_compose_yml_file:
+        server.add_dir('.', '/app')
 
-# distribute IP addresses
-kt.configure(verbose=True)
-# pass server IP to the clients
-for client in clients:
-    client.add_options(extra_hosts={'whoami.local': server.get_ip()})
-# save lab
-kt.render('/home/demo/labs/compose', verbose=True)
+    # create a local network that will connect all those nodes
+    routers = net.generate_nodes('router', 3)
+    net.create_subnet('subnet1', (routers[0], *clients[:3]))
+    net.create_subnet('subnet2', (routers[1], *clients[3:]))
+    net.create_subnet('subnet3', routers)
+    net.create_subnet('subnet4', (routers[2], server))
+
+    net.create_overlay(Overlay.RIP, 'RIP', routers)
+
+    # distribute IP addresses
+    net.configure(verbose=True)
+    # pass server IP to the clients
+    for client in clients:
+        client.add_options(extra_hosts={'whoami.local': server.get_ip()})
+    # save lab
+    net.render('/home/demo/labs/compose', verbose=True)
+
+
+if __name__ == "__main__":
+    main()
