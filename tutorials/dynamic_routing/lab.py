73a5b50f5a1ce7615fff56a3d2e2f756031b63ed
Dmitry Selivanov <st022657@student.spbu.ru>
2019-11-21 20:30:30 +0300

    refactor

--- tutorials/dynamic_routing/lab.py
+++ tutorials/dynamic_routing/lab.py
@@ -1,19 +1,26 @@
 from madt_lib.network import Network, Overlay
-kt = Network('15.0.0.0/8')
 
-server = kt.create_node('server', image='madt/nginx')
-clients = kt.generate_nodes('client', 6, image='inutano/wget',
-                            entrypoint='sh -c "while true; do wget -O - -T 3 $SERVER; sleep 1; done"')
 
-routers = kt.generate_nodes('router', 3)
-kt.create_subnet('subnet1', (routers[0], *clients[:3]))
-kt.create_subnet('subnet2', (routers[1], *clients[3:]))
-kt.create_subnet('subnet3', routers)
-kt.create_subnet('subnet4', (routers[2], server))
+def main():
+    madt = Network('15.0.0.0/8')
 
-kt.create_overlay(Overlay.RIP, 'RIP', routers)
+    server = madt.create_node('server', image='madt/nginx')
+    clients = madt.generate_nodes('client', 6, image='inutano/wget',
+                                  entrypoint='sh -c "while true; do wget -O - -T 3 $SERVER; sleep 1; done"')
 
-kt.configure(verbose=True)
-for client in clients:
-    client.add_options(environment={'SERVER': server.get_ip()})
-kt.render('/home/demo/labs/dynamic_routing', verbose=True)
+    routers = madt.generate_nodes('router', 3)
+    madt.create_subnet('subnet1', (routers[0], *clients[:3]))
+    madt.create_subnet('subnet2', (routers[1], *clients[3:]))
+    madt.create_subnet('subnet3', routers)
+    madt.create_subnet('subnet4', (routers[2], server))
+
+    madt.create_overlay(Overlay.RIP, 'RIP', routers)
+
+    madt.configure(verbose=True)
+    for client in clients:
+        client.add_options(environment={'SERVER': server.get_ip()})
+    madt.render('../../labs/dynamic_routing', verbose=True)
+
+
+if __name__ == "__main__":
+    main()
