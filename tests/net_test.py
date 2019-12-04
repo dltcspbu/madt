25bdcb0c741d62293c5e0ad14819c719b3254972
Anna Fatkina <fatkina.a.i@gmail.com>
2019-12-04 11:53:15 +0300

    unit tests

--- tests/net_test.py
+++ tests/net_test.py
@@ -4,6 +4,7 @@ import requests
 from quart import Quart
 import asyncio
 import json
+import os
 
 import madt_ui
 import madt_ui.config
@@ -21,7 +22,11 @@ def _test_app():
 
 
 class TestClass:
+
     def setup_method(self):
+        self.passwd = 'demo'
+        os.environ["SSH_PWD"] = self.passwd
+        self.already_set = []
         self.corrupt = 10
         self.delay = 20
         self.delay_distro = 1
@@ -32,51 +37,84 @@ class TestClass:
         self.rate = 1
         self.rate_unit = 'Gbps'
         self.reorder = 70
+        self.test_client = app.test_client()
+        self.container = dc.containers.run("alpine", detach=True, entrypoint="tail -f /dev/null")
         self.json_ans  = {'rate':self.rate, 'rate_unit': self.rate_unit,
                                    'delay-distro':self.delay_distro,
                                    'delay-distro_unit':self.delay_distro_unit,
                                    'loss': self.loss, 'corrupt': self.corrupt, 
                                    'reorder': self.reorder, 'duplicate': self.duplicate, 
                                    'delay': self.delay, 'delay_unit': self.delay_unit}
-    @pytest.mark.asyncio
-    async def test_tcset(self):
 
-        test_client = app.test_client()
-        c = dc.containers.run("alpine", detach=True, entrypoint="tail -f /dev/null")
-        response = await test_client.post('/login',form={'username': 'demo', 'password': 'demo'})
+    def teardown_module(self):
+        self.container.remove(force=True)
+
+    async def login(self):
+        response = await self.test_client.post('/login',form={'username': 'demo', 'password': self.passwd})
         assert response.status_code == 302
 
-        response = await test_client.post('/tcset',
-                                          query_string={'id': c.short_id},
-                                          form = self.json_ans)
+    async def tcset_param(self, param_name):
+        status = await self.login()
+        response = await self.test_client.post('/tcset',
+                                          query_string={'id': self.container.short_id},
+                                          form = {param_name: self.json_ans[param_name]})
         assert response.status_code == 200
         
-        response = await test_client.get('/tcget', query_string={'id': c.short_id})
+        response = await self.test_client.get('/tcget', query_string={'id': self.container.short_id})
         assert response.status_code == 200
 
         rj_answer = (await response.get_json())
-        assert  (rj_answer['rate'] == self.json_ans['rate'] and 
-                 rj_answer['corrupt'] == self.json_ans['corrupt'] and
-                 rj_answer['delay'] == self.json_ans['delay'] and
-                 rj_answer['rate_unit'] == self.json_ans['rate_unit'] and
-                 rj_answer['loss'] == self.json_ans['loss'] and
-                 rj_answer['duplicate'] == self.json_ans['duplicate'] and
-                 rj_answer['delay_unit'] == self.json_ans['delay_unit'] and
-                 rj_answer['delay-distro'] == self.json_ans['delay-distro'] and
-                 rj_answer['delay-distro_unit'] == self.json_ans['delay-distro_unit'] and
-                 rj_answer['reorder'] == self.json_ans['reorder'] 
-                )
+        self.already_set.append(param_name)
 
-        c.remove(force=True)
+        print('\n\n sent: ', self.json_ans)
+        print('\n\n received: ', rj_answer)
+        print('\n\n already_set: ', self.already_set)
 
+        assert all([rj_answer[k] == self.json_ans[k] for k in self.already_set])
 
-"""
-@pytest.mark.asyncio
-async def test_tcget(madt_ui):
-    datastring = "rate=&rate_unit=Kbps&delay=0&delay_unit=msec&delay-distro=0&delay-distro_unit=msec&loss=27&corrupt=0&duplicate=0&reorder=0"
-    c_id = '26069c607154'
-    test_client = app.test_client()
-    response = await test_client.get('/tcget?id=26069c607154')
-    res = await response.get_json()
-    assert res == datastring 
-"""
+    @pytest.mark.asyncio
+    async def test_login(self):
+        status = await self.login()
+
+    @pytest.mark.asyncio
+    async def test_tcset(self):
+
+        stcode = await self.login()
+        response = await self.test_client.post('/tcset',
+                                          query_string={'id': self.container.short_id},
+                                          form = self.json_ans)
+        assert response.status_code == 200
+        
+        response = await self.test_client.get('/tcget', query_string={'id': self.container.short_id})
+        assert response.status_code == 200
+
+        rj_answer = (await response.get_json())
+        assert all([rj_answer[k] == self.json_ans[k]] 
+                for k in ['rate', 'corrupt', 'delay', 'rate_unit', 'loss', 
+                          'duplicate', 'delay_unit', 'delay-distro', 'delay-distro_unit', 'reorder'])
+
+    @pytest.mark.asyncio
+    async def test_tcsee_corrupt(self):
+        status = await self.tcset_param(param_name='corrupt')
+
+    @pytest.mark.asyncio
+    async def test_tcsee_delay_n_dublicate(self):
+        status = await self.tcset_param(param_name='delay')
+        status = await self.tcset_param(param_name='duplicate')
+
+    @pytest.mark.asyncio
+    async def test_tcsee_loss(self):
+        status = await self.tcset_param(param_name='loss')
+
+    @pytest.mark.asyncio
+    async def test_tcsee_delay(self):
+        status = await self.tcset_param(param_name='delay')
+
+    @pytest.mark.asyncio
+    async def test_tcsee_dublicate(self):
+        status = await self.tcset_param(param_name='duplicate')
+
+    @pytest.mark.asyncio
+    async def test_tcsee_rate(self):
+        status = await self.tcset_param(param_name='rate')
+
