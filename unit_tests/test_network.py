4dedd60855ade1f0e9b5bd854d34e1ea97331484
Dmitry Selivanov <st022657@student.spbu.ru>
2019-12-06 20:38:32 +0300

    updated unit tests for madt_lib/network.py and added type/value checks for create/generate nodes functions

--- unit_tests/test_network.py
+++ unit_tests/test_network.py
@@ -1,6 +1,7 @@
 import unittest
 from madt_lib.network import Network
 
+
 class TestNetworkInit(unittest.TestCase):
 
 	main_net = '192.168.0.0/28'
@@ -30,6 +31,31 @@ class TestNetworkNodeManagement(unittest.TestCase):
 		node_name = 'test_node'
 		self.network.create_node(node_name)
 		self.assertTrue(node_name in [node.name for node in self.network.nodes])
+
+		with self.assertRaises(TypeError):
+			self.network.create_node(0)
+
+		with self.assertRaises(Exception):
+			self.network.create_node(node_name)
+
+	def test_local_naming(self):
+		test_net = Network(self.main_net, local=True, gateway=None, name='test_network')
+		test_net.create_node('test_node')
+		self.assertTrue(test_net.name + '_' + 'test_node' in [node.name for node in test_net.nodes])
+
+	def test_generate_nodes(self):
+		self.network.generate_nodes(prefix='test', num=10)
+		for i in range(0, 10):
+			self.assertTrue('test' + str(i+1) in [node.name for node in self.network.nodes])
+
+		with self.assertRaises(TypeError):
+			self.network.generate_nodes(prefix=0, num=10)
+
+		with self.assertRaises(TypeError):
+			self.network.generate_nodes(prefix='test1', num='test_test')
+
+		with self.assertRaises(ValueError):
+			self.network.generate_nodes(prefix='test2', num=-10)
 
 
 if __name__ == '__main__':
